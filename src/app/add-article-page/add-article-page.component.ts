b92b00a74a930810ffbfd336841d428e687fce2e
Elizaveta Ivashkova <Elizaveta_Ivashkova@epam.com>
2020-01-31 17:18:55 +0300

    Create components,routing and buttons

---
+++ src/app/add-article-page/add-article-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-article-page',
+  templateUrl: './add-article-page.component.html',
+  styleUrls: ['./add-article-page.component.scss']
+})
+export class AddArticlePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/add-edit-article-form/add-edit-article-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-article-form',
+  templateUrl: './add-edit-article-form.component.html',
+  styleUrls: ['./add-edit-article-form.component.scss']
+})
+export class AddEditArticleFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
